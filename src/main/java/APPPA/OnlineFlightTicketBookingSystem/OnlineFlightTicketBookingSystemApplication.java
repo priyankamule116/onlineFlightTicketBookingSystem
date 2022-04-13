19958413a27f6cc7222fec9ec1f3f94ab6e12c0a
unknown <priyankamule.116@gmail.com>
2022-04-13 18:45:12 +0530

    Provide comment

---
+++ src/main/java/APPPA/OnlineFlightTicketBookingSystem/OnlineFlightTicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package APPPA.OnlineFlightTicketBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFlightTicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFlightTicketBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/APPPA/OnlineFlightTicketBookingSystem/OnlineFlightTicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package APPPA.OnlineFlightTicketBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFlightTicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
